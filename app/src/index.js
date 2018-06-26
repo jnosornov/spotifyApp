956a8b19108d074143e1b5ba2bc169727c67d9e3
Johnathan <jnosornov@gmail.com>
2018-06-26 06:53:08 -0500

    Setup a configuration structure

---
+++ app/src/index.js
@@ -0,0 +1,9 @@
+import '../assets/spotify_icon.png';
+import '../stylesheets/styles.scss';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app.jsx';
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// > Root app
+const app_folder = path.resolve(__dirname, './app');
+
+// > Distribution
+const dist_folder = path.resolve(__dirname, './dist');
+const bundle_file = 'bundle.js';
+
+// > Source
+const src_folder = path.resolve(app_folder, './src');
+const entry_file = path.resolve(src_folder, './index');
+
+module.exports = {
+	mode: 'development',
+	entry: entry_file,
+	output: {
+		path: dist_folder,
+		filename: bundle_file,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)?$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.(css|sass|scss)?$/,
+				use: ['style-loader','css-loader', 'sass-loader']
+			},
+			{
+				test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+				loader: 'file-loader?name=[name].[ext]'  // <-- retain original file name
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 3000
+    },
+    plugins: [ new HtmlWebpackPlugin({
+		template: './app/index.html'
+		}) 
+	]
+}
